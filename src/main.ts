91a157a538d1055168a9f59653b0b338176e46d6
Roman <t89875041999@yandex.ru>
2024-08-13 09:28:00 +0300

    Upd: add_origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const PORT = process.env.PORT || 7777;
     app.enableCors({
-        origin: ['http://192.168.3.60:3000', 'http://localhost:3000'],
+        origin: `${process.env.LINK_ORIGIN}`,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Укажите необходимые методы
         allowedHeaders: ['Content-Type', 'Authorization'], // Укажите допустимые заголовки
         credentials: true,
